19d56aca5f29c1921bda133d6b134c447c1d7a35
Marc-Assaad Rachoine <marcassaad.rachoine@essec.edu>
2022-11-29 16:50:43 +0100

    load_csv

--- dynamic_portfolio/utils.py
+++ dynamic_portfolio/utils.py
@@ -2,9 +2,11 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
+gold = yf.download('GC=F', start='2000-01-01', end='2022-11-23')
+us_dollar = yf.download('DX-Y.NYB',start='2000-01-01', end='2022-11-23')
 
 
-def load_csv(ticker: str, start: str, end: str, percentage_change :bool= False):
+def load_csv(ticker: str, start: str, end: str):
     """
     Creating a function that would allow us to load a csv containing all relevant features of a given ticker
     ticker should be in capital
@@ -12,9 +14,18 @@ def load_csv(ticker: str, start: str, end: str, percentage_change :bool= False):
     """
 
     #Loading all relevant csv files
-    ticker = pd.read_csv(f'../raw_data/adj_return_{ticker}.csv', index_col=0)
-    gold = yf.download('GC=F', start=start, end=end)
-    us_dollar = yf.download('DX-Y.NYB', start=start, end=end)
+    ticker = pd.read_csv(f'../raw_data/adj_return_{ticker}.csv')
+    ticker.rename(columns={'timestamp':'date'}, inplace=True)
+    ticker.sort_values('date', ascending=True, inplace=True)
+    ticker.reset_index(inplace=True, drop=True)
+
+    if '1999-12-31' in ticker['date'].values:
+        ticker = ticker[ticker['date']>'1999-12-31']
+        ticker.reset_index(inplace=True, drop=True)
+
+
+    gold = gold
+    us_dollar = us_dollar
 
     credit_spread = pd.read_csv('../raw_data/us_yields.csv', index_col=0)
     oil = pd.read_csv('../raw_data/oil_return.csv', index_col=0)
@@ -28,13 +39,12 @@ def load_csv(ticker: str, start: str, end: str, percentage_change :bool= False):
     gdp_capita = pd.read_csv('../raw_data/gdp_per_capita.csv', index_col=0)
 
     #Creating our final df and merging with relevant files
-    #Still missing the returns of relevant stocks
-
-    final_df = credit_spread.copy()
+    final_df = ticker.copy()
+    final_df = final_df.merge(credit_spread, how='outer')
 
     final_df = final_df.merge(oil, how='outer', left_on='date', right_on='Date')
     final_df.drop(columns='Date', inplace=True)
-    final_df.rename(columns={'Price':'Oil_price'}, inplace=True)
+    final_df.rename(columns={'Price':'oil_price'}, inplace=True)
 
     final_df = final_df.merge(orders, how='outer')
 
@@ -67,16 +77,6 @@ def load_csv(ticker: str, start: str, end: str, percentage_change :bool= False):
     final_df['non_farm_payroll'] = final_df['non_farm_payroll'].fillna(method='ffill')
     final_df['inf_exp'] = final_df['inf_exp'].fillna(method='ffill')
     final_df['unemployment_rate'] = final_df['unemployment_rate'].fillna(method='ffill')
-
-    if percentage_change==True:
-        final_df['oil_change'] = final_df['Oil_price'].pct_change()
-        final_df['orders_change'] = final_df['orders'].pct_change()
-        final_df['unemployment_change'] = final_df['unemployment_rate'].pct_change()
-        final_df['inf_exp_change'] = final_df['inf_exp'].pct_change()
-        final_df['payroll_change'] = final_df['non_farm_payroll'].pct_change()
-        final_df['CPI_change'] = final_df['CPI'].pct_change()
-        final_df['retail_sales_change'] = final_df['retail_sales'].pct_change()
-        final_df['gdp_change'] = final_df['gdp_per_capita'].pct_change()
 
     #Finally dropping our null values since they represent weekends and holidays
     final_df.dropna(inplace=True)
