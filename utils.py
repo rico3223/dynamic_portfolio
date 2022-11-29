2cab52ae78eb6fda432d7dabafa4aae0f4e6f32d
Eric Vincent <ericvincent.escp@gmail.com>
2022-11-29 18:18:03 +0100

    macro done

--- utils.py
+++ utils.py
@@ -14,3 +14,25 @@ def load_csv(tickers: list, start:str, end:str):
     csv = csv[1:].copy()
 
     return csv
+
+
+def macro_cleaning(url):
+    """Methodology used to clean macro features extracted from Alphavantage"""
+    url = 'path'
+
+    temp = pd.read_csv(url) #reads csv as dataframe
+
+    temp['timestamp'] = pd.to_datetime(temp['timestamp']) #converts column to date format
+
+    temp = temp.rename(columns = {'timestamp':'date', 'value': 'indicator'}) #renames columns as date and the relevant indicator
+
+    temp_mask = temp['date'] >= '2000-01-01' #filter for relevant timeframe
+    temp = temp[temp_mask] #apply filter
+
+    temp = temp.sort_values('date', ascending=True) #sort to start from 2000
+
+    temp['indicator_change'] = temp['indicator'].pct_change() #calculate % changes
+
+    temp.to_csv('new_path')
+
+    return
