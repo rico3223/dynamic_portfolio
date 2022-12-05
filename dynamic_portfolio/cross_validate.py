83133bf14d6c0ac253b687ee89a48f739f67e905
Marc-Assaad Rachoine <marcassaad.rachoine@essec.edu>
2022-12-05 11:14:34 +0100

    commit solved ready to train

--- dynamic_portfolio/cross_validate.py
+++ dynamic_portfolio/cross_validate.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from dynamic_portfolio.params import cross_val
 
+
 #Split the dataset by FOLDS
 def get_folds(
     df: pd.DataFrame,
@@ -27,7 +28,7 @@ def get_folds(
 
 
 #Split FOLDS by Train et Test
-#### FOR ONE FOLDS !!!!!
+#### FOR ONE FOLD !!!!!
 def train_test_split(fold: pd.DataFrame,
                      train_test_ratio: float,
                      input_length: int,
@@ -45,88 +46,20 @@ def train_test_split(fold: pd.DataFrame,
 
     # TEST SET
     # ======================
-    first_test_idx = last_train_idx - input_length
+    first_test_idx = last_train_idx - input_length # faut il pas changer le - en + ??
     fold_test = fold.iloc[first_test_idx:, :]
 
     return (fold_train, fold_test)
 
-
-#One sequences
-### Use in tht next one function
-def get_Xi_yi(first_index: int,
-              fold: pd.DataFrame,
-              horizon: int,
-              input_length: int,
-              output_length: int):
-    '''
-    - extracts one sequence from a fold
-    - returns a pair (Xi, yi) with:
-        * len(Xi) = `input_length` and Xi starting at first_index
-        * len(yi) = `output_length`
-        * last_Xi and first_yi separated by the gap = horizon -1
-    '''
-
-    Xi_start = first_index
-    Xi_last = Xi_start + input_length
-    yi_start = Xi_last + horizon - 1
-    yi_last = yi_start + output_length
-
-    Xi = fold[Xi_start:Xi_last]
-    yi = fold[yi_start:yi_last][TARGET]
-
-    return (Xi, yi)
+# est ce que les train et test set n s'entremelent pas ?
+# changer le ticker BF-B dans la fonction tickers
 
 
-#Many sequences
-def get_X_y(fold: pd.DataFrame,
-            horizon: int,
-            input_length: int,
-            output_length: int,
-            stride: int,
-            shuffle=True):
-    """
-    - Uses `data`, a 2D-array with axis=0 for timesteps, and axis=1 for (targets+covariates columns)
-    - Returns a Tuple (X,y) of two ndarrays :
-        * X.shape = (n_samples, input_length, n_covariates)
-        * y.shape =
-            (n_samples, output_length, n_targets) if all 3-dimensions are of size > 1
-            (n_samples, output_length) if n_targets == 1
-            (n_samples, n_targets) if output_length == 1
-            (n_samples, ) if both n_targets and lenghts == 1
-    - You can shuffle the pairs (Xi,yi) of your fold
-    """
 
-    X = []
-    y = []
-
-    for i in range(0, len(fold), stride):
-        ## Extracting a sequence starting at index_i
-        Xi, yi = get_Xi_yi(first_index=i,
-                           fold=fold_test,
-                           horizon=horizon,
-                           input_length=input_length,
-                           output_length=output_length)
-        ## Exits loop as soon as we reach the end of the dataset
-        if len(yi) < output_length:
-            break
-        X.append(Xi)
-        y.append(yi)
-
-    X = np.array(X)
-    y = np.array(y)
-    y = np.squeeze(y)
-
-    if shuffle:
-        idx = np.arange(len(X))
-        np.random.shuffle(idx)
-        X = X[idx]
-        y = y[idx]
-
-    return X, y
 
 
 #Loop for split on FOLDS
-def cross_validate_dl() :
+def cross_validate_dl(df:pd.DataFrame, fold_length:int, fold_stride:int) :
     '''
     get_folds() create many FOLDS, train_test_split() create a split on ONE FOLDS.
     The goal of this function is to make splits and sequences on each FOLDS.
@@ -160,7 +93,7 @@ def cross_validate_dl() :
         #Rajouter le modèle
 
 
-def cross_validate_ml() :
+def cross_validate_ml(df:pd.DataFrame, fold_length:int, fold_stride:int) :
     '''
     get_folds() create many FOLDS, train_test_split() create a split on ONE FOLDS.
     The goal of this function is to make splits and sequences on each FOLDS.
