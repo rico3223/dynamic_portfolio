7a1fbca817500ca3eb3cdf600de0370b22a9bfe8
Marc-Assaad Rachoine <marcassaad.rachoine@essec.edu>
2022-12-08 16:26:23 +0100

    unfiinished backtest

--- dynamic_portfolio/portfolio_backtest.py
+++ dynamic_portfolio/portfolio_backtest.py
@@ -7,28 +7,35 @@ import datetime as dt
 
 class Portfolio():
 
-    def __init__(self, capital, tax_rate, fee):
+    def __init__(self, capital, tax_rate, fee, percentage):
         self.capital = capital
         self.tax_rate = tax_rate
         self.fee = fee
+        self.percentage = percentage
         self.trades = 0
         self.daily_return = 0
         self.overall_return = 0
         self.prices = pd.read_csv('raw_data/backtest_data/open_price_454_stocks.csv', index_col=0)
         self.prices.reset_index(inplace=True, drop=True)
-        self.preds = pd.read_csv('raw_data/backtest_data/gradientboosting_pred_454_stocks.csv', index_col=0)
-        self.preds.reset_index(inplace=True, drop=True)
 
-    def rotation_strategy(self, n_longs, n_shorts):
+    def rotation_strategy(self, n_longs, n_shorts, y_pred=True):
         self.n_longs = n_longs
         self.n_shorts = n_shorts
         self.shorts = []
         self.longs = []
         self.counter = 0
-        for self.row in self.preds.index[:len(self.preds)-2]:
+
+        if y_pred==True:
+            self.returns = pd.read_csv('raw_data/backtest_data/gradientboosting_pred_454_stocks.csv', index_col=0)
+            self.returns.reset_index(inplace=True, drop=True)
+        else:
+            self.returns = pd.read_csv('raw_data/backtest_data/real_returns.csv', index_col=0)
+            self.returns.reset_index(inplace=True, drop=True)
+
+        for self.row in self.returns.index[:len(self.returns)-1]:
             self.counter += 1
             print(f'Day {self.counter}\n=================')
-            self.value = self.preds.loc[self.row].sort_values(ascending=True)
+            self.value = self.returns.loc[self.row].sort_values(ascending=True)
             self.close_position()
             print('Closed positions')
             for stock in self.value.index[:self.n_shorts]:
@@ -36,7 +43,6 @@ class Portfolio():
                     self.place_buy_order(stock = stock)
                 if stock not in self.shorts and stock in self.value.index[:self.n_shorts]:
                     self.place_sell_order(stock = stock)
-                    self.shorts.append(stock)
                 if stock in self.shorts and stock in self.value.index[:self.n_shorts]:
                     continue
             for stock in self.value.index[-self.n_longs:]:
@@ -44,16 +50,22 @@ class Portfolio():
                     self.place_sell_order(stock = stock)
                 if stock not in self.longs and stock in self.value.index[-self.n_longs:]:
                     self.place_buy_order(stock = stock)
-                    self.longs.append(stock)
                 if stock in self.longs and stock in self.value.index[-self.n_longs:]:
                     continue
             print('Opened poistions')
+
         # Close out all positions
-        print(self.longs)
-        print(self.shorts)
+        for stock in self.longs:
+            self.place_sell_order(stock=stock)
+            self.longs.remove(stock)
+        for stock in self.shorts:
+            self.place_buy_order(stock=stock)
+            self.shorts.remove(stock)
+        print(self.longs, self.shorts)
+
 
     def close_position(self):
-        for stock in self.preds.columns:
+        for stock in self.returns.columns:
             if stock in self.shorts and stock in self.value.index[self.n_shorts:-self.n_longs+1]:
                 self.place_buy_order(stock=stock)
                 self.shorts.remove(stock)
@@ -62,20 +74,28 @@ class Portfolio():
                 self.longs.remove(stock)
 
 
-    def place_buy_order(self, stock, units = 1):
-        price = self.prices.at[self.row+1, stock] # aller récupérer le bon prix
+    def place_buy_order(self, stock):
+        price = self.prices.at[self.row+1, stock]
+        units = int((self.percentage*self.capital)/price)
         self.capital -= (units * price) * (1 + self.fee)
-        self.trades += 1
 
-        print(f'buy {stock} at price={price}: capital={self.capital}, trades={self.trades}')
-        #\n portfolio buy = {self.longs}
+        for i in range(units):
+            self.longs.append(stock)
+
+        self.trades += 1
+        print(f'buy {units} {stock} at price={price} capital={self.capital}, trades={self.trades}')
 
     def place_sell_order(self, stock, units = 1):
-        price = self.prices.at[self.row+1, stock] # aller récupérer le bon prix
+        price = self.prices.at[self.row+1, stock]
+        units = int((self.percentage*self.capital)/price)
         self.capital += (units * price) * (1 - self.fee)
+
+        for i in range(units):
+            self.shorts.append(stock)
+
         self.trades += 1
-        print(f'sell {stock} at price={price}: capital={self.capital}, trades={self.trades}')
+        print(f'sell {units} {stock} at price={price}: capital={self.capital}, trades={self.trades}')
 
 if __name__== "__main__":
-    toto = Portfolio(100000, 0, 0)
-    toto.rotation_strategy(n_longs=5, n_shorts=5)
+    toto = Portfolio(100000, 0, 0, percentage=0.05)
+    toto.rotation_strategy(n_longs=5, n_shorts=5, y_pred=False)
